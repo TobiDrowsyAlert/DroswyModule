713a5f6401e70d14a49a426f7ade0e0639c38146
jh-sun <slsjemf@gmail.com>
2020-03-11 23:12:47 +0900

    눈 깜빡임, 하품, 얼굴 각도 감지를 병합한 데모

--- face_detect_merge.py
+++ face_detect_merge.py
@@ -6,8 +6,90 @@ import imutils
 import time
 import dlib
 import cv2
+import math
 from sleep_step_calcaulation import sleep_step_calc
 
+#눈 좌표의 비율을 계산하는 함수
+def eye_aspect_ratio(eye):
+    #눈의 종방향 좌표의 차를 계산
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    #눈의 횡방향 좌표의 차를 계산
+    C = dist.euclidean(eye[0], eye[3])
+
+    #ear 공식을 이용하여 계산
+    ear = (A + B) / (2.0 * C)
+
+    return ear
+
+#입 크기 비율 계산 함수
+def mouth_aspect_ratio(mouth):
+    A=dist.euclidean(mouth[14],mouth[18])
+    B=dist.euclidean(mouth[12],mouth[16])
+
+    ear=A/B
+
+    return ear
+
+#얼굴 각도 계산 함수
+def pose_aspect_angle(rect, landmarks):
+
+    #2D points
+    image_points = np.array(
+        [
+            (landmarks[30][0], landmarks[30][1]),  # nose tip
+            (landmarks[8][0], landmarks[8][1]),  # chin
+            (landmarks[36][0], landmarks[36][1]),  # left eye left corner
+            (landmarks[45][0], landmarks[45][1]),  # right eye right corner
+            (landmarks[48][0], landmarks[48][1]),  # left mouth corner
+            (landmarks[54][0], landmarks[54][1])  # right mouth corner
+        ],
+        dtype="double",
+    )
+
+    #3D model points
+    model_points = np.array(
+        [
+            (0.0, 0.0, 0.0),             # nose tip
+            (0.0, -330.0, -65.0),        # chin
+            (-165.0, 170.0, -135.0),     # left eye left corner
+            (165.0, 170.0, -135.0),      # right eye right corner
+            (-150.0, -150.0, -125.0),    # left mouth corner
+            (150.0, -150.0, -125.0)      # right mouth corner
+        ]
+    )
+
+
+    (x,y,w,h)=face_utils.rect_to_bb(rect)
+
+    #print("in func :: x:%f,y:%f",x + w / 2, y + h / 2)
+    center=(x+w/2,y+h/2)
+    focal_length = center[0] / np.tan(60 / (2 * np.pi / 180))
+    camera_matrix = np.array(
+        [
+            [focal_length, 0, center[0]],
+            [0, focal_length, center[1]],
+            [0, 0, 1]
+        ],
+        dtype="double",
+    )
+
+    dist_coeffs = np.zeros((4, 1))
+    _, r_vec, trans_vec = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
+
+    r_vector_matrix = cv2.Rodrigues(r_vec)[0]
+
+    project_matrix = np.hstack((r_vector_matrix, trans_vec))
+    euler_angles = cv2.decomposeProjectionMatrix(project_matrix)[6]
+
+    pitch, yaw, roll = [math.radians(_) for _ in euler_angles]
+
+    pitch = math.degrees(math.asin(math.sin(pitch)))
+    roll = -math.degrees(math.asin(math.sin(roll)))
+    yaw = math.degrees(math.asin(math.sin(yaw)))
+
+    return int(pitch), int(roll), int(yaw)
 
 #dlib에서 얼굴 식별을 위한 함수 호출
 print("[INFO] loading facial landmark predictor...")
